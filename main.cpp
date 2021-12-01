5e61c0edd0ff8829c9563088452ad197771f9f30
riccardocurci <91483368+riccardocurci@users.noreply.github.com>
2021-12-01 23:02:15 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,41 @@
 #include <iostream>
 
-int main(){
-  return 0;
+
+bool primo (int x){
+
+    for (int i = 2; i < x; ++i) {
+
+        if(x%i==0){
+
+            return false;
+        }
+
+    }
+    return true;
+
+}
+
+
+
+int main() {
+
+
+
+    int x;
+
+    std::cin >> x;
+
+    bool r = primo(x);
+
+    if(primo(x)== true){
+
+        std::cout << "numero primo" << std::endl;
+
+    }else{
+
+        std::cout << "numero non primo" << std::endl;
+
+    }
+
+    return 0;
 }
